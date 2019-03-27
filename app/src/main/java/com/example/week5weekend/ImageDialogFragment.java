869ae322d5b18f4a4dca2038f8d4a39bb62daba7
Marco Carrizales <marco.carrizales@mobileappscompany.us>
2019-03-26 23:56:44 -0400

    Dialogs

---
+++ app/src/main/java/com/example/week5weekend/ImageDialogFragment.java
@@ -0,0 +1,61 @@
+package com.example.week5weekend;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+
+import static android.content.DialogInterface.BUTTON_NEGATIVE;
+import static android.content.DialogInterface.BUTTON_POSITIVE;
+
+public class ImageDialogFragment extends DialogFragment
+        implements DialogInterface.OnClickListener, DialogInterface.OnDismissListener{
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            ImageDialogFragment.this.dismiss();
+        }
+    };
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.dialog_with_image, null))
+                .setPositiveButton(R.string.ok, this)
+                .setNegativeButton(R.string.cancel, this);
+
+        return builder.create();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+            // Doesn't matter which button was pressed
+            case BUTTON_POSITIVE:
+            case BUTTON_NEGATIVE:
+                ImageDialogFragment.this.getDialog().cancel();
+                break;
+        }
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        Log.d("Log.d", "Dialog Dismissed");
+        handler.removeCallbacks(runnable);
+    }
+
+    public void dismissAfterSeconds(int seconds){
+        handler.postDelayed(runnable, seconds*1000);
+    }
+}
--- app/src/main/java/com/example/week5weekend/MainActivity.java
+++ app/src/main/java/com/example/week5weekend/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.week5weekend;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +26,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void dialogWithImage(View view) {
+        final ImageDialogFragment dialogFragment = new ImageDialogFragment();
+        dialogFragment.show(getSupportFragmentManager(), "imageDialog");
 
+        dialogFragment.dismissAfterSeconds(3);
+    }
+
+    public void alertDialogNormal(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.normal_alert_dialog)
+                .setMessage("This is a normal Alert Dialog")
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+        builder.create().show();
+    }
+
+    public void alertDialogCustom(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.alert_dialog_custom, null));
+        builder.create().show();
+    }
+
+    public void alertDialogWithList(View view) {
+        // The font of this alert dialog is on purpose
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.alert_dialog_list_title)
+                .setItems(R.array.alert_dialog_options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String[] array = getResources().getStringArray(R.array.alert_dialog_options);
+                        Toast.makeText(MainActivity.this, "You have selected " + array[which], Toast.LENGTH_SHORT).show();
+                    }
+                });
+        builder.create().show();
     }
 }
